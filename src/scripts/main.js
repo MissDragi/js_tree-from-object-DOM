f0eb8c91debf517c80057cb86dac1cbd7e1f9544
Drsgon <oksanaleschenko.ol@gmail.com>
2025-07-08 15:10:04 +0300

    add tfod  task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const createdTree = document.createElement('div');
+
+createdTree.id = 'tree';
+document.body.appendChild(createdTree);
+
 const food = {
   Drink: {
     Wine: {},
@@ -22,6 +27,35 @@ const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
   // WRITE YOUR CODE HERE
+  const ul = document.createElement('ul');
+
+  element.appendChild(ul);
+
+  for (const key in food) {
+    const li = document.createElement('li');
+
+    li.textContent = key;
+    ul.appendChild(li);
+
+    for (const subKey in food[key]) {
+      const subUl = document.createElement('ul');
+      const subLi = document.createElement('li');
+
+      subLi.textContent = subKey;
+      subUl.appendChild(subLi);
+      li.appendChild(subUl);
+
+      for (const subSubKey in food[key][subKey]) {
+        const subSubLi = document.createElement('li');
+        const subSubUl = document.createElement('ul');
+
+        subLi.appendChild(subSubUl);
+        subSubLi.textContent = subSubKey;
+
+        subSubUl.appendChild(subSubLi);
+      }
+    }
+  }
 }
 
 createTree(tree, food);
